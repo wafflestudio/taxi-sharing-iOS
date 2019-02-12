c0a87ffabf399f42afdc1d4ac4a1954937aebe86
croonjohn <tmdduf430@naver.com>
2019-02-12 19:26:58 +0900

    first commit

---
+++ taxi-sharing/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  taxi-sharing
+//
+//  Created by 오승열 on 12/02/2019.
+//  Copyright © 2019 CroonSSS. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
