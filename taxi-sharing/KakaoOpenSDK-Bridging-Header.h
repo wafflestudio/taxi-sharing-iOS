e4b0f1eca8a8e502e7802ab8d6cc071110b03f80
croonjohn <tmdduf430@naver.com>
2019-03-09 13:15:02 +0900

    bridging header added for kakao API

---
+++ taxi-sharing/KakaoOpenSDK-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  KakaoOpenSDK-Bridging-Header.h
+//  taxi-sharing
+//
+//  Created by 오승열 on 09/03/2019.
+//  Copyright © 2019 CroonSSS. All rights reserved.
+//
+
+#ifndef KakaoOpenSDK_Bridging_Header_h
+#define KakaoOpenSDK_Bridging_Header_h
+
+#import <KakaoOpenSDK/KakaoOpenSDK.h>
+
+#endif /* KakaoOpenSDK_Bridging_Header_h */
+
