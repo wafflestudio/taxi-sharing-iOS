a3b45471af8933f3ecb91cf886432c4e581be9e4
croonjohn <tmdduf430@naver.com>
2019-05-09 14:07:23 +0900

    TOCropViewController for cropping images added

--- taxi-sharing/CropViewController.swift
+++ taxi-sharing/CropViewController.swift
@@ -8,16 +8,23 @@
 
 import UIKit
 
-class CropViewController: TOCrop {
-    func presentCropViewController() {
-        var image: UIImage? // Load an image
-        let cropViewController = CropViewController(croppingStyle: .circular, image: image)
-        cropViewController.delegate = self
-        self.present(cropViewController, animated: true, completion: nil)
+class CropViewController: TOCropViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
     }
     
-    func cropViewController(_ cropViewController: TOCropViewController?, didCropToCircularImage image: UIImage?, with cropRect: CGRect, angle: Int) {
-        // 'image' is the newly cropped, circular version of the original image
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
     }
+    */
 
 }
